38bce3afe886b7ffc4b3ec5303a6efbbb021fc67
Pacífico Escobar <68441691+pacificoescobar@users.noreply.github.com>
2025-02-24 08:58:21 -0300

    fix build capacitor 7

--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,23 @@
-import { WebPlugin } from '@capacitor/core';
+import { WebPlugin, PluginListenerHandle } from '@capacitor/core';
 
 import type { CallKitVoipPlugin } from './definitions';
 
 export class CallKitVoipWeb extends WebPlugin implements CallKitVoipPlugin {
   async register(): Promise<void> {
     console.log('call is register');
-    return;
+  }
+
+  addListener(
+    eventName: string,
+    listenerFunc: (...args: any[]) => void
+  ): Promise<PluginListenerHandle> & PluginListenerHandle {
+    const listenerHandle = super.addListener(eventName, listenerFunc);
+
+    return Object.assign(listenerHandle, {
+      remove: async () => {
+        const resolvedHandle = await listenerHandle;
+        resolvedHandle.remove();
+      },
+    });
   }
 }
